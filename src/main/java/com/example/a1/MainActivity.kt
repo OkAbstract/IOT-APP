15729469b7a725a4bec63aac7862181756fa4375
OkAbstract <72602829+OkAbstract@users.noreply.github.com>
2022-12-16 19:12:55 +0400

    Add files via upload

---
+++ src/main/java/com/example/a1/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.a1
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import com.example.a1.R
+import android.os.Bundle
+import android.view.View
+import android.widget.*
+
+class MainActivity : AppCompatActivity() {
+
+    /**
+     * A Login Form Example in Kotlin Android
+     */
+    class MainActivity : AppCompatActivity() {
+
+//        @SuppressLint("MissingInflatedId")
+//        override fun onCreate(savedInstanceState: Bundle?) {
+//            super.onCreate(savedInstanceState)
+//            setContentView(R.layout.activity_main)
+//
+//            // get reference to all views
+//            val et_user_name = findViewById(R.id.et_user_name) as TextView
+//            val et_password = findViewById(R.id.et_password) as EditText
+//            val btn_reset = findViewById(R.id.btn_reset) as Button
+//            val btn_submit = findViewById(R.id.btn_submit) as Button
+//
+//            btn_reset.setOnClickListener {
+//                // clearing user_name and password edit text views on reset button click
+//                et_user_name.setText("")
+//                et_password.setText("")
+//            }
+//
+//            // set on-click listener
+//            btn_submit.setOnClickListener {
+//                val user_name = et_user_name.text
+//                et_password.text
+//                Toast.makeText(this@MainActivity, user_name, Toast.LENGTH_LONG).show();
+
+
+            }
+        }
+
